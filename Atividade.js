9931ae7ec5e6b22a8970e4c4a5f14f3bd61eaf17
André <andre.ljs197153@gmail.com>
2024-09-29 21:58:04 +0000

    atividades.js

--- Atividade.js
+++ Atividade.js
@@ -1,6 +1,10 @@
 var nome = " André";
 var idade = " 53 ";
-console.log ("Olá meu nome é"  + nome +  "e tenho"  + idade +  "anos.")
+console.log ("Olá meu nome é"  + nome +  "e tenho"  + idade +  "anos.");
+
+var nome = "SeuNome";
+var cidade = " Sua Cidade ";
+console.log(nome + "é de" + cidade +'.');
 
 
 
